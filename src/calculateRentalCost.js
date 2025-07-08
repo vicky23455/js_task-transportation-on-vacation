fa66d23f69c0f80712c69f7f743c90b984677b69
Viktoriia <everydayvikusia@gmail.com>
2025-07-08 19:42:35 +0200

    added capital consts

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const rentCost = 40;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    return rentCost * days - 50;
+  if (days >= LONG_TERM) {
+    return PRICE_PER_DAY * days - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return rentCost * days - 20;
+  if (days >= SHORT_TERM) {
+    return PRICE_PER_DAY * days - SHORT_TERM_DISCOUNT;
   }
 
-  return rentCost * days;
+  return PRICE_PER_DAY * days;
 }
 
 module.exports = calculateRentalCost;
